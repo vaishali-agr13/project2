e09a07cc0545bc58bd0be10a4f517a13a52b397a
vaishali-agr13 <vaishali.agr13@gmail.com>
2026-04-14 19:31:25 +0530

    responsive-design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,22 @@
 <body class="bg-[#0f172a] text-white">
 
   <!-- Navbar -->
-  <header class="flex justify-between items-center px-10 py-5">
-    <h1 class="text-2xl font-bold text-white">JobPortal</h1>
-    <nav class="space-x-6">
-      <a href="/" class="hover:text-blue-400">Home</a>
-      <a href="/find-jobs" class="hover:text-blue-400">Find a Job</a>
-      <a href="#" class="hover:text-blue-400">Companies</a>
-      <a href="#" class="hover:text-blue-400">Login</a>
-    </nav>
-  </header>
+  <header class="flex flex-wrap justify-between items-center px-6 py-5">
+  
+  <!-- Logo -->
+  <h1 class="text-2xl font-bold text-white w-full md:w-auto">
+    JobPortal
+  </h1>
+
+  <!-- Menu -->
+  <nav class="flex flex-wrap gap-4 mt-4 md:mt-0">
+    <a href="/" class="hover:text-blue-400">Home</a>
+    <a href="/find-jobs" class="hover:text-blue-400">Find a Job</a>
+    <a href="#" class="hover:text-blue-400">Companies</a>
+    <a href="#" class="hover:text-blue-400">Login</a>
+  </nav>
+
+</header>
 
   @yield('content')
 
